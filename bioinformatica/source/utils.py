be41d0c7107e1080a616beac678e54fce3b8d727
federicostacchietti <federico.stacchietti@gmail.com>
2020-06-03 15:32:42 +0200

    models refactoring added models functions functions to transform data

--- bioinformatica/source/utils.py
+++ bioinformatica/source/utils.py
@@ -5,11 +5,11 @@ from ucsc_genomes_downloader import Genome
 from keras_bed_sequence import BedSequence
 from keras_mixed_sequence import MixedSequence
 
-def load_dataset(cell_line, window_size):
+def load_dataset(cell_line, window_size, type):
     epigenomes, labels = load_epigenomes(
         cell_line=cell_line,
         dataset='fantom',
-        regions='enhancers',
+        regions=type,
         window_size=window_size
     )
     labels = labels.values.ravel()
