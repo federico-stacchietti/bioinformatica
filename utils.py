0950ad025ac530b35992ddd2131f227701ab2828
flaviofuria <furia.flavio@outlook.it>
2020-06-03 10:16:15 +0200

    init

---
+++ utils.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from epigenomic_dataset import load_epigenomes
+from ucsc_genomes_downloader import Genome
+from keras_bed_sequence import BedSequence
+from keras_mixed_sequence import MixedSequence
+
+def load_dataset(cell_line, window_size):
+    epigenomes, labels = load_epigenomes(
+        cell_line=cell_line,
+        dataset='fantom',
+        regions='enhancers',
+        window_size=window_size
+    )
+    labels = labels.values.ravel()
+    return epigenomes, labels
+
