73cb3c76196bccc50ead6f994a855577b323005c
federicostacchietti <federico.stacchietti@gmail.com>
2020-06-03 12:12:27 +0200

    created data decomposition function updated global variable random_state minor changes in check_balance function

--- bioinformatica/source/preprocessing.py
+++ bioinformatica/source/preprocessing.py
@@ -2,7 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
 from sklearn.utils import resample, shuffle
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 
+random_state = 1
 
 def detect_NaN(epigenomes):
     return sum(sample.isna().values.sum() for _, sample in epigenomes)
@@ -25,8 +28,16 @@ def check_balance(epigenomes, labels):
         majority_class = epigenomes.iloc[np.where(labels != minority_label)]
         # trova i sample della classe sbilanciata, gli fa il resample e lo unisce alla classe non sbilanciata, poi fa lo shuffle
         epigenomes = shuffle(pd.concat([majority_class, resample(epigenomes.iloc[np.where(labels == minority_label)],
-                            random_state=0, n_samples=max_unbalance - minority_count)], axis=0), random_state=0)
+                            random_state=random_state, n_samples=max_unbalance - minority_count)], axis=0), random_state=random_state)
 
-def drop_constant_feature(epigenomes):
-    return epigenomes.loc[:, (epigenomes != epigenomes.iloc[0]).any()]
+def data_decomposition(epigenomes, label):
+    decompositions = []
+    algorithms = [PCA, TSNE]
+    n_components = 2
+    for algorithm in algorithms:
+        decomposition = algorithm(n_components=n_components, random_state=random_state)
+        decompositions.append(decomposition.fit_transform(epigenomes))
+    pass
+
+
 
