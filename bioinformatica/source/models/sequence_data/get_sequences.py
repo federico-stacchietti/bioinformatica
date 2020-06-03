658c0e0a87af88ecc47c6cb736b5c8f3f3348ee7
federicostacchietti <federico.stacchietti@gmail.com>
2020-06-03 15:36:13 +0200

    models refactoring added models functions functions to transform data

---
+++ bioinformatica/source/models/sequence_data/get_sequences.py
@@ -0,0 +1,5 @@
+from keras_bed_sequence import BedSequence
+from keras_mixed_sequence import MixedSequence
+
+def epigenomic_to_data(epigenomes, labels):
+    pass
---
+++ bioinformatica/source/models/training.py
@@ -0,0 +1,11 @@
+from multiprocessing import Process
+
+
+from bioinformatica.source.models.definition import define_models
+
+
+def train(dataset, labels):
+    models = define_models()
+    for model in models:
+        pass
+
