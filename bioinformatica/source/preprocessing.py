b0924b07dc3642138df6a2961df7de7d8632b5a8
flaviofuria <furia.flavio@outlook.it>
2020-06-03 17:33:58 +0200

    preprocessing functions added

--- bioinformatica/source/preprocessing.py
+++ bioinformatica/source/preprocessing.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import numpy as np
 from sklearn.impute import KNNImputer
+from sklearn.preprocessing import RobustScaler
 from sklearn.utils import resample, shuffle
 
 
 def detect_NaN(epigenomes):
     return sum(sample.isna().values.sum() for _, sample in epigenomes)
 
-def imputation(epigenomes):
+def imputation(epigenomes:pd.DataFrame)->pd.DataFrame:
     if detect_NaN(epigenomes) > 0:
         epigenomes = pd.DataFrame(
             KNNImputer(n_neighbors=20).fit_transform(epigenomes.values),
@@ -28,7 +29,7 @@ def check_balance(epigenomes, labels):
                             random_state=0, n_samples=max_unbalance - minority_count)], axis=0), random_state=0)
 
 # Input:dataframe. Controlla la presenza di feature costanti. Se presenti, le elimina dal dataframe e lo restituisce
-def drop_constant_features(epigenomes):
+def drop_constant_features(epigenomes:pd.DataFrame)->pd.DataFrame:
     const_features = [feature for feature in epigenomes.columns if epigenomes[feature].nunique() == 1]
     non_const_features = [feature for feature in epigenomes.columns if feature not in const_features]
     if not non_const_features:
@@ -36,6 +37,15 @@ def drop_constant_features(epigenomes):
         # print('there is no constant feature in the dataset')
     else:
         return(epigenomes[non_const_features])
+
+# Input:dataframe. Versione robusta dello z-scoring applicato a tutto il dataset: ad ogni valore sottraggo la media e divido per la deviazione standard
+def robust_zscoring(df:pd.DataFrame)->pd.DataFrame:
+    return pd.DataFrame(
+        RobustScaler().fit_transform(df.values),
+        columns=df.columns,
+        index=df.index
+    )
+
 
 
 
