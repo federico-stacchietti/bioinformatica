b880dfa21e31b6493452837d54e1f03a0d93db0e
federicostacchietti <federico.stacchietti@gmail.com>
2020-06-03 15:36:13 +0200

    models refactoring added models functions functions to transform data

---
+++ bioinformatica/source/models/definition.py
@@ -0,0 +1,16 @@
+from sklearn import svm as SVM
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input, Flatten
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import BatchNormalization, Dropout, Activation
+from tensorflow.keras.layers import Conv2D, Reshape
+from tensorflow.keras.layers import LSTM
+
+
+def define_models():
+    models = []
+    return models
