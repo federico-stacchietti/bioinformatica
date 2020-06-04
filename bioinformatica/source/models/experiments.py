f4bb6a008e55ec036178628c1beb3a16e4cb8720
federicostacchietti <federico.stacchietti@gmail.com>
2020-06-04 12:14:36 +0200

    experiment class refactoring

--- bioinformatica/source/models/experiments.py
+++ bioinformatica/source/models/experiments.py
@@ -1,15 +1,6 @@
 from bioinformatica.source.data_initialization.datasets_initialization import get_data
 
-from sklearn import svm as SVM
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import balanced_accuracy_score, balanced_accuracy_score, roc_auc_score, average_precision_score
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input, Flatten
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import BatchNormalization, Dropout, Activation
-from tensorflow.keras.layers import Conv2D, Reshape
-from tensorflow.keras.layers import LSTM
 
 from tensorflow.keras.metrics import AUC
 
