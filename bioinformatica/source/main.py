911dbb6ba07fd65bd460b114c844f512fb9bf4c9
steecccc <federico.stacchietti@gmail.com>
2020-06-09 10:06:47 +0200

    refactoring

--- bioinformatica/source/main.py
+++ bioinformatica/source/main.py
@@ -2,6 +2,4 @@ from bioinformatica.source.models_builder.experiments_builder.builder import *
 
 
 if __name__ == '__main__':
-    dicctt = {'a': [1, 2], 'b': [5, 6]}
-
-    print([[x] + dicctt.get(x) for x in dicctt])
+    pass
