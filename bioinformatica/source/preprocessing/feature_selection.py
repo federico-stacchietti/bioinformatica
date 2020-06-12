ffa9046734276e5a9e106c8dfb2767ef75dbdd12
steecccc <federico.stacchietti@gmail.com>
2020-06-12 14:21:32 +0200

    refactoring added preprocessing pipelines functions

--- bioinformatica/source/preprocessing/feature_selection.py
+++ bioinformatica/source/preprocessing/feature_selection.py
@@ -6,7 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from multiprocessing import cpu_count
 
 
-def boruta_filter(dataset: pd.DataFrame, labels: np.array, max_iter: int, p_value_threshold: float, random_state: int) \
+def boruta(dataset: pd.DataFrame, labels: np.array, max_iter: int, p_value_threshold: float, random_state: int) \
         -> pd.DataFrame:
     forest = RandomForestClassifier(n_jobs=cpu_count(), class_weight='balanced', max_depth=5)
     boruta_selector = BorutaPy(
@@ -20,6 +20,6 @@ def boruta_filter(dataset: pd.DataFrame, labels: np.array, max_iter: int, p_valu
     return boruta_selector.fit_transform(dataset.values, labels)
 
 
-def boostaroota_filter(dataset : pd.DataFrame, labels : np.array) -> pd.DataFrame:
-    br = BoostARoota(metric='logloss')
+def boostaroota(dataset: pd.DataFrame, labels: np.array) -> pd.DataFrame:
+    br = BoostARoota(metric='logloss', silent=True)
     return br.fit_transform(dataset, labels)
