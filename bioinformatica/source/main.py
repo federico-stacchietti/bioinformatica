4e0a2e82256232ed2445f9d62599c1b2e12a0e30
federico <federico.stacchietti@gmail.com>
2020-07-01 12:24:31 +0200

    bug fixing and refactoring

--- bioinformatica/source/main.py
+++ bioinformatica/source/main.py
@@ -6,16 +6,15 @@ if __name__ == '__main__':
     # pass
     experiment_id = 2
     data_type = 'sequences'
-    cell_line, window_size, typez = 'K562', 200, 'enhancers'
-    n_split, test_size, random_state = 3, 0.2, 1
-    balance = None
+    cell_line, window_size, epigenomic_type = 'K562', 200, 'enhancers'
+    n_split, test_size, random_state = 1, 0.2, 1
+    balance = 'under_sample'
     defined_algorithms = define_models()
     holdout_parameters = (n_split, test_size, random_state)
-    data_parameters = ((cell_line, window_size, typez), data_type)
+    data_parameters = ((cell_line, window_size, epigenomic_type), data_type)
     alphas = [0.05]
     experiment = Experiment(experiment_id, data_parameters, holdout_parameters, alphas, defined_algorithms, balance)
     experiment.execute()
     experiment.evaluate()
-    # experiment.print_model_info('all')
-    experiment.results_to_dataframe()
-    experiment.results_to_dataframe()
+    experiment.print_model_info('all')
+    # experiment.results_to_dataframe()
