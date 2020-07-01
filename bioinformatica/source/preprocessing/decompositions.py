eb0a7a31bca7a58831e2fca770cc0c76e0c6d57f
federico <federico.stacchietti@gmail.com>
2020-07-01 15:45:56 +0200

    refactoring

--- bioinformatica/source/preprocessing/decompositions.py
+++ bioinformatica/source/preprocessing/decompositions.py
@@ -10,7 +10,9 @@ def PCA_function(dataset: pd.DataFrame or np.array, n_components: int = 50, rand
 
 
 def TSNE_function(dataset: pd.DataFrame or np.array, n_components: int = 2, random_state: int = 0,
-                  perplexity: int = 30) -> pd.DataFrame or np.array:
-    dataset = PCA_function(dataset)
+                  perplexity: int = 30, PCA_before: bool = False, PCA_n_components: int = 50) \
+        -> pd.DataFrame or np.array:
+    if PCA_before:
+        dataset = PCA_function(dataset, PCA_n_components, random_state=random_state)
     return TSNE(n_components=n_components, perplexity=perplexity, random_state=random_state)\
         .fit_transform(dataset)
--- bioinformatica/source/visualizations/visualization.py
+++ bioinformatica/source/visualizations/visualization.py
@@ -5,7 +5,9 @@ import os
 
 def make_visualization(visualization_type: str, dataset: pd.DataFrame or np.array = None, labels: np.array = None,
                        cell_line: str = None, epigenomic_type: str = None, dataset_type: str = None,
-                       top_feature_distribution: int = None):
+                       top_feature_distribution: int = None, top_features_for_correlation: int = None,
+                       p_value_correlation: float = None, PCA_before_TSNE: bool = True, PCA_n_components: int = 50,
+                       TSNE_n_components: int = 2, TSNE_perplexity: int = 30, random_state: int = 1):
     if visualization_type == 'experiment_results':
         path = Path(__file__).parent.parent
         experiment_files = os.listdir(str(path) + '/experiments/results')
@@ -17,10 +19,10 @@ def make_visualization(visualization_type: str, dataset: pd.DataFrame or np.arra
         else:
             print('No experiment has been made, so any result can\'t be plotted')
     elif visualization_type == 'PCA':
-        dataset = PCA_function(dataset)
+        dataset = PCA_function(dataset, PCA_n_components, random_state)
         PCA_TSNE_visualization(cell_line + '_' + epigenomic_type + dataset_type, dataset, labels, 'PCA')
     elif visualization_type == 'TSNE':
-        dataset = TSNE_function(dataset)
+        dataset = TSNE_function(dataset, TSNE_n_components, random_state, TSNE_perplexity, PCA_before_TSNE, PCA_n_components)
         PCA_TSNE_visualization(cell_line + '_' + epigenomic_type + dataset_type, dataset, labels, 'TSNE')
     elif visualization_type == 'balancing':
         ones = np.count_nonzero(labels == 1)
@@ -32,7 +34,7 @@ def make_visualization(visualization_type: str, dataset: pd.DataFrame or np.arra
     elif visualization_type == 'feature_correlations':
         features = feature_correlation(dataset)
         feature_correlations_visualization(cell_line + '_' + epigenomic_type + '_correlation.png', dataset,
-                                           features, labels, 3, 0.01)
+                                           features, labels, top_features_for_correlation, p_value_correlation)
     elif visualization_type == 'feature_distribution':
         feature_distribution_visualization(cell_line + '_' + epigenomic_type + '_feature_distribution.png',
                                            dataset, labels, top_feature_distribution)
