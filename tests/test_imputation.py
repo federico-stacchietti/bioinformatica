c86a277e844fa2c4152762bb9b0e7a801cc55480
federico <federico.stacchietti@gmail.com>
2020-07-01 21:03:44 +0200

    tests updated

--- tests/test_imputation.py
+++ tests/test_imputation.py
@@ -12,17 +12,6 @@ def test_nan_check():
     assert dataset is not None, 'error while executing nan check'
 
 
-def test_nan_filter():
-    parameters = ('K562', 200, 'enhancers'), 'epigenomic'
-    data = get_data(parameters)
-    dataset, labels = data
-
-    dataset = dataset.head(100)
-    labels = labels[:100]
-    dataset, labels = nan_filter(dataset, labels)
-    assert dataset is not None, 'error while filterning nans from dataset'
-
-
 def test_imputation():
     parameters = ('K562', 200, 'enhancers'), 'epigenomic'
     data = get_data(parameters)
